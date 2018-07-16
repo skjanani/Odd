ff69a395dedbf60c837485c758b2000bb64827da
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-16 17:59:43 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,15 @@
+package janani;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10;i<=20;i++){
+			if(i%2!=0){
+				System.out.print(i+" ");
+
+	}
+
+}
+	}
+}
